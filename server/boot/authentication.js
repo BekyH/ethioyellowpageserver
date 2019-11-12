d729dca5466a5587009d06b89b690e113f6f3845
unknown <bereketheramo25@gmail.com>
2019-11-12 14:55:10 +0300

    modifications

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,11 +1,3 @@
-// Copyright IBM Corp. 2016. All Rights Reserved.
-// Node module: loopback-workspace
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-'use strict';
-
-module.exports = function enableAuthentication(server) {
-  // enable authentication
-  server.enableAuth();
-};
+module.exports = function enableAuthentication(server){
+    server.enableAuth();
+}
